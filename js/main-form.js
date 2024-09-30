53b0bf41c05903718e5b383aa640890381ea8379
AHerrera23 <herreraabel892@gmail.com>
2024-09-29 21:42:21 -0300

    version deskr beta

--- js/main-form.js
+++ js/main-form.js
@@ -2,29 +2,31 @@ const btn = document.getElementById("button");
 const modal = document.getElementById("modal");
 let enviar = false;
 
-
-document.getElementById("form").addEventListener("submit", function (event) {
-  event.preventDefault();
-  btn.value = "Sending...";
-  const nombre = document.getElementById("to_name").value;
-  const nombreModal = document.getElementById("h3");
-  if(enviar===true){
-    document.body.style.overflow = 'hidden';
-    modal.style.display='flex'
-    nombreModal.innerHTML = nombre
-  }
-  const serviceID = "default_service";
-  const templateID = "template_8wksmow";
-
-  emailjs.sendForm(serviceID, templateID, this).then(
-    () => {
-      btn.value = "Send Email";
-      enviar=true
-      //alert('Sent!');
-    },
-    (err) => {
-      btn.value = "Send Email";
-      alert(JSON.stringify(err));
+const enviarForm = ()=>{
+  document.getElementById("form").addEventListener("submit", function (event) {
+    event.preventDefault();
+    btn.value = "Sending...";
+    const nombre = document.getElementById("to_name").value;
+    const nombreModal = document.getElementById("h3");
+    if(enviar===true){
+      document.body.style.overflow = 'hidden';
+      modal.style.display='flex'
+      nombreModal.innerHTML = nombre
     }
-  );
-});
+    const serviceID = "default_service";
+    const templateID = "template_8wksmow";
+  
+    emailjs.sendForm(serviceID, templateID, this).then(
+      () => {
+        btn.value = "Send Email";
+        enviar=true
+        //alert('Sent!');
+      },
+      (err) => {
+        btn.value = "Send Email";
+        alert(JSON.stringify(err));
+      }
+    );
+  });
+  
+};
